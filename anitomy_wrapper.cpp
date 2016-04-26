4572011c6af301f9e7abb2a75de5d538d1f217d0
nevermnd <thiago.oliveira.gt14@gmail.com>
2016-04-25 23:30:18 -0300

    Debug info on anitomy_wrapper

--- anitomy_wrapper.cpp
+++ anitomy_wrapper.cpp
@@ -1,5 +1,6 @@
 #include <nan.h>
 #include <anitomy/anitomy.h>
+#include <iostream>
 
 void Test(const Nan::FunctionCallbackInfo<v8::Value>& args) {
     v8::Isolate* isolate = args.GetIsolate();
@@ -7,6 +8,18 @@ void Test(const Nan::FunctionCallbackInfo<v8::Value>& args) {
     anitomy::Anitomy anitomy;
     anitomy.Parse(L"[Ouroboros]_Fullmetal_Alchemist_Brotherhood_-_01.mkv");  
 
+    auto& elements = anitomy.elements();
+
+    for (auto& element : elements) {
+        std::wcout << element.first << L"\t" << element.second << std::endl;
+    }
+    std::wcout << std::endl;
+
+    // You can access values directly by using get() and get_all() methods
+    std::wcout << elements.get(anitomy::kElementAnimeTitle)    << L" #"   <<
+                  elements.get(anitomy::kElementEpisodeNumber) << L" by " <<
+                  elements.get(anitomy::kElementReleaseGroup)  << std::endl;
+                
     args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "Hello world!"));
 }
 
