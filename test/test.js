02cb5b82ffdda59b4fc314f56be7433de0dcf8e2
Thiago Oliveira <thiago.oliveira.gt14@gmail.com>
2016-05-09 16:20:19 -0300

    finally some tests!

--- test/test.js
+++ test/test.js
@@ -6,14 +6,11 @@
  * MIT License <https://github.com/nevermnd/anitomy-js/blob/master/LICENSE>
  ********************************************************************/
 
-// TODO add more tests for options
-
-var assert = require('chai').assert;
+// TODO add tests for ignored_strings and allowed_delimiters
 var expect = require('chai').expect;
-
 var anitomy = require('../build/Release/anitomy-js');
 
-describe('anitomy', function () {
+describe('anitomy-js', function () {
 
     var fixture = require('./data.json');
     var fixtureValues = [];
@@ -29,6 +26,21 @@ describe('anitomy', function () {
 
         it('should parse all anime filenames synchronously, all at once', function () {
             expect(anitomy.parseSync(fixtureKeys)).to.deep.equal(fixtureValues);
+        });
+
+        it('should parse anime filenames sync ignoring episode number, title, file extension and release group', function () {
+            var parsed = anitomy.parseSync(fixtureKeys, {
+                "parse_episode_title": false,
+                "parse_episode_number": false,
+                "parse_file_extension": false,
+                "parse_release_group": false
+            });
+            parsed.forEach(function (anime) {
+                expect(anime).to.not.have.property("episode_title");
+                expect(anime).to.not.have.property("episode_number_alt");
+                expect(anime).to.not.have.property("file_extension");
+                expect(anime).to.not.have.property("release_group");
+            });
         });
 
         it('should throw an exception for wrong datatypes (files)', function () {
@@ -58,6 +70,25 @@ describe('anitomy', function () {
             });
         });
 
+        it('should parse anime filenames async ignoring episode number, title, file extension and release group', function (done) {
+            var options = {
+                "parse_episode_title": false,
+                "parse_episode_number": false,
+                "parse_file_extension": false,
+                "parse_release_group": false
+            };
+            anitomy.parseAsync(fixtureKeys, function (data) {
+                data.forEach(function (anime) {
+                    expect(anime, anime.file_name).to.not.have.property("episode_title");
+                    expect(anime, anime.file_name).to.not.have.property("episode_number_alt");
+                    expect(anime, anime.file_name).to.not.have.property("episode_number");
+                    expect(anime, anime.file_name).to.not.have.property("file_extension");
+                    expect(anime, anime.file_name).to.not.have.property("release_group");
+                });
+                done();
+            }, options);
+        });
+
         it('should throw an exception for wrong datatypes (files)', function () {
             expect(function () { anitomy.parseAsync(1, function () { }); }).to.throw('Wrong data type');
         });
@@ -65,10 +96,9 @@ describe('anitomy', function () {
         it('should throw an exception for wrong datatypes (callback)', function () {
             expect(function () { anitomy.parseAsync("", null); }).to.throw('Second parameter must be a callback');
         });
-        
+
         it('should throw an exception for wrong datatypes (options)', function () {
             expect(function () { anitomy.parseAsync("", function () { }, null); }).to.throw('Options must be an object');
         });
     });
-
 });
