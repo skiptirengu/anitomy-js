a9f7cb29f9ab2ece327fc4f0bed09a8a080a0181
Thiago Oliveira <thiago_ogt@outlook.com>
2018-08-10 14:51:56 -0300

    New parseAsync method - Deprecate old callback style - Added new callback style - Added promise api

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 'use strict'
 
-const anitomy = require('./build/Release/anitomy-js')
+var anitomy = require('./build/Release/anitomy-js')
+var util = require('util')
 
 /**
  * @param {array|string} input 
+ * @param {function} callback
  * @param {object} options 
- * @returns {Promise<object>}
+ * @returns {Promise<object>|undefined}
  */
-function parseAsync (input, options) {
-  return new Promise((resolve, reject) => {
+function parseAsync(input, callback, options) {
+
+  if (typeof callback === 'object') {
+    options = callback
+    callback = null
+  }
+
+  if (callback && callback.length === 1) {
+    var newCallback = util.deprecate(
+      callback,
+      'Using a callback with only one argument is deprecated. Switch to using a node style callback (err, data) or use the promise api.', 'anitomy-js#parseAsync'
+    )
+    return anitomy.parseAsync(input, newCallback, options)
+  }
+
+  var promise = new Promise(function (resolve, reject) {
     try {
       anitomy.parseAsync(input, resolve, options)
     } catch (err) {
       reject(err)
     }
   })
+
+  return promise
+    .then(function (parsed) {
+      if (callback) {
+        callback(undefined, parsed)
+      } else {
+        return parsed
+      }
+    })
+    .catch(function (err) {
+      if (callback) {
+        callback(err)
+      } else {
+        throw err
+      }
+    })
 }
 
 /**
@@ -22,11 +54,13 @@ function parseAsync (input, options) {
  * @param {object} options 
  * @returns {array|object}
  */
-function parseSync (input, options) {
+function parseSync(input, options) {
   return anitomy.parseSync(input, options)
 }
 
-module.export = {
+module.exports = {
+  parse: parseAsync,
   parseAsync,
-  parseSync
+  parseSync,
+  anitomy
 }
