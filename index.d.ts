35478a5f0a21dbbe490475a4eafd7d45e152fdf0
Thiago Oliveira <thiago_ogt@outlook.com>
2019-11-03 22:24:30 -0300

    update types and workflow

--- index.d.ts
+++ index.d.ts
@@ -1,59 +1,54 @@
-// eslint-disable
-declare module "anitomy-js" {
+declare module 'anitomy-js' {
   export interface AnitomyOptions {
-    allowed_delimiters?: string;
-    ignored_strings?: string[];
-    parse_episode_number?: boolean;
-    parse_episode_title?: boolean;
-    parse_file_extension?: boolean;
-    parse_release_group?: boolean;
+    allowed_delimiters?: string
+    ignored_strings?: string[]
+    parse_episode_number?: boolean
+    parse_episode_title?: boolean
+    parse_file_extension?: boolean
+    parse_release_group?: boolean
   }
 
   export interface AnitomyResult {
-    file_name: string;
-    anime_season?: string;
-    season_prefix?: string;
-    anime_title?: string;
-    anime_type?: string;
-    anime_year?: string;
-    audio_term?: string;
-    device_compatibility?: string;
-    episode_number?: string;
-    episode_number_alt?: string;
-    episode_prefix?: string;
-    episode_title?: string;
-    file_checksum?: string;
-    file_extension?: string;
-    language?: string;
-    other?: string;
-    release_group?: string;
-    release_information?: string;
-    release_version?: string;
-    source?: string;
-    subtitles?: string;
-    video_resolution?: string;
-    video_term?: string;
-    volume_number?: string;
-    volume_prefix?: string;
-    unknown?: string;
+    file_name: string
+    anime_season?: string
+    season_prefix?: string
+    anime_title?: string
+    anime_type?: string
+    anime_year?: string
+    audio_term?: string
+    device_compatibility?: string
+    episode_number?: string
+    episode_number_alt?: string
+    episode_prefix?: string
+    episode_title?: string
+    file_checksum?: string
+    file_extension?: string
+    language?: string
+    other?: string
+    release_group?: string
+    release_information?: string
+    release_version?: string
+    source?: string
+    subtitles?: string
+    video_resolution?: string
+    video_term?: string
+    volume_number?: string
+    volume_prefix?: string
+    unknown?: string
   }
 
-  type AnitomyCallback = (
-    error: Error,
-    result: AnitomyResult[] | AnitomyResult
-  ) => void;
+  export interface Anitomy {
+    parse(input: string, options?: AnitomyOptions): Promise<AnitomyResult>
+    parseAsync(input: string, options?: AnitomyOptions): Promise<AnitomyResult>
 
-  type AnitomyAsync = (
-    input: string | string[],
-    options?: AnitomyOptions,
-    callback?: AnitomyCallback
-  ) => Promise<AnitomyResult[] | AnitomyResult> | undefined;
+    parse(input: string[], options?: AnitomyOptions): Promise<AnitomyResult[]>
+    parseAsync(input: string[], options?: AnitomyOptions): Promise<AnitomyResult[]>
 
-  type AnitomySync = (
-    input: string | string[],
-    options?: AnitomyOptions
-  ) => AnitomyResult[] | AnitomyResult;
+    parseSync(input: string, options?: AnitomyOptions): AnitomyResult
+    parseSync(input: string[], options?: AnitomyOptions): AnitomyResult[]
+  }
 
-  export const parse: AnitomyAsync;
-  export const parseSync: AnitomySync;
+  const anitomy: Anitomy
+
+  export default anitomy
 }
